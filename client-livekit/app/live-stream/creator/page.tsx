0878046488bbe1aec97325b243decea1107da266
subhankar-fleapo <subhankar@fleapo.com>
2024-04-22 17:00:09 +0530

    comments integrated

--- client-livekit/app/live-stream/creator/page.tsx
+++ client-livekit/app/live-stream/creator/page.tsx
@@ -9,19 +9,33 @@ import {
   ControlBar,
   useTracks,
 } from "@livekit/components-react";
-import { Track } from "livekit-client";
+import { Room, RoomEvent, Track } from "livekit-client";
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function Page() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
+  const [room] = useState(new Room());
 
   if (!token) {
     return <div>Getting token from backend. Please wait...</div>;
   }
 
+  const onConnection = () => {
+    console.log("HIT");
+    room.on(RoomEvent.DataReceived, (data: any) => {
+      console.log("HIT", data);
+      const decoder = new TextDecoder();
+      const decodedData = decoder.decode(data);
+      console.log(JSON.parse(decodedData));
+    });
+  };
+
   return (
     <LiveKitRoom
+      room={room}
+      onConnected={onConnection}
       video={true}
       audio={true}
       token={token}
--- client-livekit/app/live-stream/fan/page.tsx
+++ client-livekit/app/live-stream/fan/page.tsx
@@ -9,12 +9,22 @@ import {
   GridLayout,
   ParticipantTile,
 } from "@livekit/components-react";
-import { Track } from "livekit-client";
+import { Room, RoomEvent, Track } from "livekit-client";
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function Page() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
+  const [room] = useState(new Room());
+
+  const onConnection = () => {
+    room.on(RoomEvent.DataReceived, (data: any) => {
+      const decoder = new TextDecoder();
+      const decodedData = decoder.decode(data);
+      console.log(JSON.parse(decodedData));
+    });
+  };
 
   if (!token) {
     return <div>Getting token from backend. Please wait...</div>;
@@ -22,6 +32,8 @@ export default function Page() {
 
   return (
     <LiveKitRoom
+      room={room}
+      onConnected={onConnection}
       video={true}
       audio={true}
       token={token}
